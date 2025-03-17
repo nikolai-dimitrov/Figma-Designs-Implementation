1d2adced2ba288a4b13c0cec77017e9c1541223d
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-03-17 19:38:31 +0200

    responsive-startup/feat: refresh nav on media change and show/hide nav on click

--- Figma-Designs/Responsive-Startup-Website/public/js/nav.js
+++ Figma-Designs/Responsive-Startup-Website/public/js/nav.js
@@ -4,17 +4,24 @@ const matchMobileScreenMedia = window.matchMedia("(max-width: 768px)");
 const burgerMenuTopElement = document.querySelector(".burger-menu .top");
 const burgerMenuBottomElement = document.querySelector(".burger-menu .bottom");
 
+const navElement = document.querySelector(".nav")
 const handleMobileScreenMedia = () => {
 	console.log("mobile screen media");
+    burgerMenuTopElement.classList.remove("spin");
+    burgerMenuBottomElement.classList.remove("spin");
+    navElement.classList.remove("show");
 };
 
 const handleBurgerMenuClick = (e) => {
 	if (burgerMenuTopElement.classList.contains("spin")) {
 		burgerMenuTopElement.classList.remove("spin");
 		burgerMenuBottomElement.classList.remove("spin");
+        navElement.classList.remove("show");
+
 	} else {
 		burgerMenuTopElement.classList.add("spin");
 		burgerMenuBottomElement.classList.add("spin");
+        navElement.classList.add("show");
 	}
 };
 
